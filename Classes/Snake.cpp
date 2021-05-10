ec310df0740c083d09afbe25cda8040fe0968e1f
V <platinumcool777@gmail.com>
2021-05-10 12:33:32 +0300

    Initialize snake class, addition of tail func

--- Classes/Snake.cpp
+++ Classes/Snake.cpp
@@ -5,7 +5,9 @@
 #include "Snake.h"
 
 void Snake::addTailPart() {
-
+    auto last = *(tailParts.end()-1);
+    auto newGridPosition = step(std::get<0>(last),std::get<2>(last), true);
+    tailParts.push_back(std::make_tuple(newGridPosition,scene->makeSnakeTail(newGridPosition.first,newGridPosition.second),std::get<2>(last)));
 }
 
 void Snake::removeTailPart() {
@@ -20,6 +22,28 @@ void Snake::move() {
 
 }
 
-Snake::Snake() {
+Snake::Snake(int row, int column, Way way, GardenScene* scene) {
+    this->scene = scene;
+    tailParts.push_back(std::make_tuple(std::make_pair(row,column),scene->makeSnakeTail(row,column),way));
+    addTailPart();
+}
 
+std::pair<int, int> Snake::step(std::pair<int,int> start, Way way, bool isInverse) {
+    std::pair<int, int> stepWay(0,0);
+    switch (way) {
+        case Way::RIGHT:
+            stepWay = std::make_pair(1,0);
+            break;
+        case Way::LEFT:
+            stepWay = std::make_pair(-1,0);
+            break;
+        case Way::UP:
+            stepWay = std::make_pair(0,1);
+            break;
+        case Way::DOWN:
+            stepWay = std::make_pair(0,-1);
+            break;
+    }
+    if(isInverse) stepWay = std::make_pair(-stepWay.first, -stepWay.second);
+    return std::make_pair(start.first+stepWay.first,start.second+stepWay.second);
 }
--- Classes/Snake.h
+++ Classes/Snake.h
@@ -6,17 +6,27 @@
 #define PROJ_ANDROID_SNAKE_H
 #include <vector>
 #include <cocos2d.h>
-
+#include "GardenScene.h"
+enum Way {
+    UP=0,
+    DOWN,
+    LEFT,
+    RIGHT
+};
 class Snake {
-    std::vector<std::pair<int,int>> tailCoords;
-    std::vector<cocos2d::Node> tailNodes;
+    std::vector<std::tuple<std::pair<int,int>,cocos2d::Node*,Way>> tailParts;
+    std::pair<int,int> step(std::pair<int,int> start, Way way, bool isInverse = false);
+
+    GardenScene* scene;
 public:
-    Snake();
+    Snake(int row, int column, Way way, GardenScene* scene);
     void addTailPart();
     void removeTailPart();
 
     void recalculateMatrix();
     void move();
+
+
 };
 
 
